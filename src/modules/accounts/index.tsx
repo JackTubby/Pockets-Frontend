2f78a6b0b7e2fbc63bcdae527f7da60b33a44053
Jack Tubby <jack200034@hotmail.com>
2024-07-14 13:03:28 +0100

    Update account cards styling & add inital summary section

--- src/modules/accounts/index.tsx
+++ src/modules/accounts/index.tsx
@@ -1,23 +1,10 @@
 import { useEffect } from "react";
 import useFetchAccounts from "./hooks/useFetchAccounts";
 import Navbar from "../core/components/navbar";
-import { DataView } from "primereact/dataview";
+import { Card } from "primereact/card";
 
 const Index = () => {
   const { fetchAccounts, data: accounts, isLoading, isError, error } = useFetchAccounts();
-
-  const listItemTemplate = (account) => {
-    return (
-      <div className="p-col-12 p-md-3">
-        <div>
-          <h3>{account.bankName}</h3>
-          <p>Account Holder: {account.name}</p>
-          <p>Balance: {account.balance}</p>
-          <p>Last Updated: {new Date(account.updatedAt).toLocaleDateString()}</p>
-        </div>
-      </div>
-    );
-  };
 
   useEffect(() => {
     fetchAccounts();
@@ -35,17 +22,44 @@ const Index = () => {
     <>
       <Navbar />
       <br />
-      <DataView value={accounts || []} itemTemplate={listItemTemplate} layout="list" paginator rows={5} />
+      {/* BANK ACCOUNTS */}
+      <div className="overflow-x-auto flex gap-x-6">
+        {accounts?.map((account) => (
+          <Card key={account.id} className="min-w-[420px] md:w-[30rem] flex flex-col my-6 hover:cursor-pointer hover:">
+            <div className="flex gap-x-2 h-full">
+              <div className="w-[1rem] bg-red-500"></div>
+              <div className="flex flex-col justify-between h-full gap-y-2 p-4">
+                <div className="flex gap-x-4 items-center">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBkW0cqU0FCIoWOvAiuqdnt6WCRkxiBRNUgA&s"
+                    alt="bank logo"
+                    width="50px"
+                    height="50px"
+                    className="border rounded-xl"
+                  />
+                  <h2>{account.bankName}</h2>
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="text-base w-[10rem]">Account Name:</span>
+                  <span className="text-lg font-bold">{account.name}</span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="text-base w-[10rem]">Current Balance:</span>
+                  <span className="text-lg font-bold">£{account.balance}</span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="text-base w-[10rem]">Last Four Digits:</span>
+                  <span className="text-lg font-bold">**** **** **** {account.digits}</span>
+                </div>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+      {/* SUMMARY */}
+      <Card title="Summary"></Card>
     </>
   );
 };
-
-// IDEAS:
-/**
- * 1. We could have an accounts slider (overflow) with cards of all the accounts essentially like we have in the home page.
- * Then a plus button where we could create more sliders below to group accounts if we wanted to.
- * Maybe have more details in the cards than the home page we do not want it to similar as it would seem pointless
- * Below the cards/sliders we could have some details/charts of the account data
- */
 
 export default Index;
