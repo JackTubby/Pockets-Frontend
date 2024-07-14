23b54bf5001158cadaa1b9ba0f94fe9860214499
Jack Tubby <jack200034@hotmail.com>
2024-07-14 13:18:17 +0100

    Add highlight color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const colors = {
+  ...defaultTheme.colors,
+  highlight: "#2DD4BF",
+};
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/primereact/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/primereact/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
   corePlugins: {
     preflight: false,
   },
-}
+};
