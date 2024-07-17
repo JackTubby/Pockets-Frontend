9b4d6c4ec5d1ef5a7174656b4b5ba8155293ce8b
Jack Tubby <jack200034@hotmail.com>
2024-07-17 09:09:34 +0100

    Create basic menu layout

--- src/modules/accounts/index.tsx
+++ src/modules/accounts/index.tsx
@@ -3,6 +3,9 @@ import useFetchAccounts from "./hooks/useFetchAccounts";
 import Navbar from "../core/components/navbar";
 import { Card } from "primereact/card";
 import AccountCard from "./components/account-card-two";
+import { SpeedDial } from "primereact/speeddial";
+import { Tooltip } from "primereact/tooltip";
+import { MenuItem } from "primereact/menuitem";
 
 const Index = () => {
   const { fetchAccounts, data: accounts, isLoading, isError, error } = useFetchAccounts();
@@ -19,6 +22,24 @@ const Index = () => {
     return <div>Error: {error.message}</div>;
   }
 
+  const menuItems: MenuItem[] = [
+    {
+      label: "Create",
+      icon: "pi pi-plus",
+      command: () => {
+        console.log("Create");
+      },
+    },
+    {
+      // Make accounts selectable & then confirm deletion
+      label: "Delete",
+      icon: "pi pi-trash",
+      command: () => {
+        console.log("Delete");
+      },
+    },
+  ];
+
   return (
     <>
       <Navbar />
@@ -31,6 +52,15 @@ const Index = () => {
       </div>
       {/* SUMMARY */}
       <Card title="Summary"></Card>
+      <SpeedDial
+        model={menuItems}
+        radius={60}
+        type="quarter-circle"
+        direction="up-left"
+        className="fixed right-10 bottom-10"
+        showIcon="pi pi-bars"
+        hideIcon="pi pi-times"
+      />
     </>
   );
 };
