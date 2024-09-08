ae509f11b09e1ff38922b69709d2530b9a017f52
Jack Tubby <jack200034@hotmail.com>
2024-09-08 13:57:19 +0100

    Update cards to display correctly and editable ability

--- src/modules/accounts/components/account-card-two.tsx
+++ src/modules/accounts/components/account-card-two.tsx
@@ -1,41 +1,82 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "primereact/card";
 import { Dialog } from "primereact/dialog";
-import { createInputConfigs, updateInputConfigs, createSchema, updateSchema } from "../../core/utils/validation-schema";
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import useEditAccount from "../hooks/useEditAccount";
 
 interface Account {
   id: string;
   data: {
     bankName: string;
     name: string;
-    balance: number;
-    digits: number;
+    balance: string;
+    digits: string;
+    color: string;
   };
 }
 
 const AccountCards = (account: Account) => {
+  const { editAccount, isPending } = useEditAccount();
   const [visible, setVisible] = useState(false);
+  const [formData, setFormData] = useState(account.data);
 
   const handleClick = () => {
     setVisible(true);
   };
 
-  const handleUpdate = (data: unknown) => {
-    console.log("Update:", data);
+  const handleInputChange = (e: { target: { name: string; value: unknown } }) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
+
+  const handleBankChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      bankName: e.value.code,
+    }));
+  };
+
+  const handleEditAccount = () => {
+    editAccount(formData);
+    setVisible(false);
+  };
+
+  // TODO: Replace this with a fetch from the server if needed
+  const bankNames = [
+    { name: "NatWest", code: "NATWEST" },
+    { name: "Nation Wide", code: "NATIONWIDE" },
+    { name: "Halifax", code: "HALIFAX" },
+    { name: "HSBC", code: "HSBC" },
+    { name: "Santander", code: "SANTANDER" },
+    { name: "Monzo", code: "MONZO" },
+  ];
+
+  const selectedBank = bankNames.find((bank) => bank.code === formData.bankName);
+
   return (
     <>
-      {/* CREATE ACCOUNT DIALOG */}
-      <Dialog
-        header="Edit Account"
-        style={{ width: "50vw" }}
-        visible={visible}
-        onHide={() => {
-          if (!visible) return;
-          setVisible(false);
-        }}
-      >
-        <h1>{account.data.name}</h1>
+      {/* EDIT ACCOUNT DIALOG */}
+      <Dialog header="Edit Account" style={{ width: "50vw" }} visible={visible} onHide={() => setVisible(false)}>
+        <h1>{formData.name}</h1>
+        <div className="flex flex-col gap-y-4">
+          <InputText name="name" value={formData.name} onChange={handleInputChange} placeholder="Account Name" />
+          <InputText name="balance" value={formData.balance} onChange={handleInputChange} placeholder="Balance" />
+          <Dropdown
+            value={selectedBank}
+            onChange={handleBankChange}
+            options={bankNames}
+            optionLabel="name"
+            placeholder="Select a Bank"
+          />
+          <InputText name="digits" value={formData.digits} onChange={handleInputChange} placeholder="Digits" />
+          <InputText name="color" value={formData.color} onChange={handleInputChange} placeholder="Color" />
+          <Button onClick={handleEditAccount} label="Update" disabled={isPending} />
+        </div>
       </Dialog>
       {/* ACCOUNT INDIVIDUAL CARD */}
       <Card
@@ -53,19 +94,19 @@ const AccountCards = (account: Account) => {
                 height="50px"
                 className="border rounded-xl"
               />
-              <h2>{account.data.bankName}</h2>
+              <h2>{formData.bankName}</h2>
             </div>
             <div className="flex justify-between items-center">
               <span className="text-base w-[10rem]">Account Name:</span>
-              <span className="text-lg font-bold">{account.data.name}</span>
+              <span className="text-lg font-bold">{formData.name}</span>
             </div>
             <div className="flex justify-between items-center">
               <span className="text-base w-[10rem]">Current Balance:</span>
-              <span className="text-lg font-bold">£{account.data.balance}</span>
+              <span className="text-lg font-bold">£{formData.balance}</span>
             </div>
             <div className="flex justify-between items-center">
               <span className="text-base w-[10rem]">Last Four Digits:</span>
-              <span className="text-lg font-bold">**** **** **** {account.data.digits}</span>
+              <span className="text-lg font-bold">**** **** **** {formData.digits}</span>
             </div>
           </div>
         </div>
---
+++ src/modules/accounts/hooks/useEditAccount.tsx
@@ -0,0 +1,48 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAppContext } from "../../core/utils/app-provider";
+import { AxiosError } from "axios";
+import { useCallback } from "react";
+import { useNavigate } from "react-router";
+
+interface EditAccountData {
+  id: string;
+  digits: string;
+  balance: string;
+  color: string;
+  name: string;
+}
+
+const useEditAccount = () => {
+  const { api } = useAppContext();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const mutation = useMutation<void, AxiosError, EditAccountData>({
+    mutationFn: async (data: EditAccountData) => {
+      console.log("Edit data:", data);
+      await api.put(`/bankaccount/${data.id}`, data);
+    },
+    onMutate: () => {
+      console.log("Creating bank account...");
+    },
+    onError: (error) => {
+      console.error("Error", error);
+      console.error("Error Message: ", (error.response?.data as { message: string })?.message);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      navigate("accounts");
+    },
+  });
+
+  const editAccount = useCallback(
+    (data: EditAccountData) => {
+      mutation.mutate(data);
+    },
+    [mutation]
+  );
+
+  return { editAccount, ...mutation };
+};
+
+export default useEditAccount;
