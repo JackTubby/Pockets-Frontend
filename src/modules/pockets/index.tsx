901bdc63a3dab2a2f188555c11be751988208cd0
Jack Tubby <jack200034@hotmail.com>
2024-09-29 11:58:37 +0100

    Create inital pockets layout

--- src/modules/pockets/index.tsx
+++ src/modules/pockets/index.tsx
@@ -1,7 +1,21 @@
+import Navbar from "../core/components/navbar";
+import PocketCard from "./components/pocket-card";
+
 const Index = () => {
   return (
-    <h1>Hello Pockets!</h1>
-  )
-}
+    <>
+      <Navbar />
+      <div className="container mx-auto">
+        <h1>Welcome to your Pockets!</h1>
+        <div className="flex flex-col gap-y-4">
+        <PocketCard />
+        <PocketCard />
+        <PocketCard />
+        <PocketCard />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Index
+export default Index;
