5b107c45a0b56ce553d523a201ebda5071d793e7
Jack Tubby <jack200034@hotmail.com>
2024-09-29 12:48:01 +0100

    Introduce props into pocket cards

--- src/modules/pockets/components/pocket-card.tsx
+++ src/modules/pockets/components/pocket-card.tsx
@@ -1,19 +1,28 @@
 import { Card } from "primereact/card";
 import { ProgressBar } from "primereact/progressbar";
 
-const PocketCard: React.FC = () => {
+interface details {
+  details: {
+    name: string;
+    balance: string;
+    message: string;
+  };
+}
+
+const PocketCard: React.FC<details> = ({ details }) => {
+  const { name, balance, message } = details;
   const icon = "🏦";
   return (
     <Card className="w-[100%] relative pb-4 cursor-pointer hover:shadow-2xl hover:shadow-gray-900">
       <div className="flex justify-between">
         <div>
           <h2>{icon}</h2>
-          <h1>House Savings 2022</h1>
+          <h1>{name}</h1>
         </div>
         <img src="https://via.placeholder.com/500" alt="" className="rounded-full" width={120} height={120} />
       </div>
-      <p className="font-bold text-xl">£1,000.00</p>
-      <p>Achieved 5% of your £300 goal!</p>
+      <p className="font-bold text-xl">{balance}</p>
+      <p>{message}</p>
       <ProgressBar value={50}></ProgressBar>
     </Card>
   );
--- src/modules/pockets/index.tsx
+++ src/modules/pockets/index.tsx
@@ -8,10 +8,10 @@ const Index = () => {
       <div className="container mx-auto">
         <h1>Welcome to your Pockets!</h1>
         <div className="flex flex-col gap-y-4">
-        <PocketCard />
-        <PocketCard />
-        <PocketCard />
-        <PocketCard />
+        <PocketCard details={{name: "Pocket name", balance: "500", message: "You are 5% away from your target"}}  />
+        <PocketCard details={{name: "Pocket name", balance: "500", message: "You are 5% away from your target"}}  />
+        <PocketCard details={{name: "Pocket name", balance: "500", message: "You are 5% away from your target"}}  />
+        <PocketCard details={{name: "Pocket name", balance: "500", message: "You are 5% away from your target"}}  />
         </div>
       </div>
     </>
