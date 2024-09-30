5bd8347018abca68b7556948a0fbd1d03408e8b9
Jack Tubby <jack200034@hotmail.com>
2024-09-30 21:57:18 +0100

    Update progress bar look

--- src/modules/pockets/components/pocket-card.tsx
+++ src/modules/pockets/components/pocket-card.tsx
@@ -1,5 +1,7 @@
 import { Card } from "primereact/card";
 import { ProgressBar } from "primereact/progressbar";
+import { useEffect, useState } from "react";
+import { Fragment } from "react/jsx-runtime";
 
 interface details {
   details: {
@@ -11,7 +13,22 @@ interface details {
 
 const PocketCard: React.FC<details> = ({ details }) => {
   const { name, balance, message } = details;
+  const [newBal, setNewBal] = useState<number>(0);
   const icon = "🏦";
+
+  const valueTemplate = (value: number) => {
+    return <Fragment>{value}%</Fragment>;
+  };
+
+  const calculateProgressBarPercentage = (balance: number, target: number) => {
+    const newBalCalc = balance / target * 100;
+    setNewBal(newBalCalc);
+  };
+
+  useEffect(() => {
+    calculateProgressBarPercentage(parseInt(balance), 1000);
+  }, [balance]);
+
   return (
     <Card className="w-[100%] relative pb-4 cursor-pointer hover:shadow-2xl hover:shadow-gray-900">
       <div className="flex justify-between">
@@ -21,9 +38,9 @@ const PocketCard: React.FC<details> = ({ details }) => {
         </div>
         <img src="https://via.placeholder.com/500" alt="" className="rounded-full" width={120} height={120} />
       </div>
-      <p className="font-bold text-xl">{balance}</p>
+      <p className="font-bold text-xl">£{balance}</p>
       <p>{message}</p>
-      <ProgressBar value={50}></ProgressBar>
+      <ProgressBar value={newBal} displayValueTemplate={valueTemplate}></ProgressBar>
     </Card>
   );
 };
